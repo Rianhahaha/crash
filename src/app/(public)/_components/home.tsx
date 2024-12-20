a689193076f94997c516aece22977837c0b9cbd9
Rianhahaha <triandiapriliio@gmail.com>
2024-12-20 21:55:47 +0700

    first

--- src/app/(public)/_components/home.tsx
+++ src/app/(public)/_components/home.tsx
@@ -7,21 +7,16 @@ export default function Home() {
         <>
 
             <section
-                className="h-screen w-full overflow-hidden relative"
-                style={{
-                    backgroundImage: "url('./assets/bg.png')",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
+                className="h-screen w-full overflow-hidden relative bg-[url('/assets/bg.png')] bg-cover bg-center"
             >
-                <Image src="/assets/art.gif" className="bottom-0 left-0 absolute " alt="" />
-                <Image src="/assets/title.png" className="top-[5%] right-[10%] absolute " alt="" />
+                <Image width={100} height={100} src="/assets/art.gif" className="w-[60rem] bottom-0 left-0 absolute " alt="" />
+                <Image width={300} height={300} src="/assets/title.png" className="w-[30rem] top-[5%] right-[10%] absolute " alt="" />
                 <div className="absolute bottom-[30%] right-[16%]  flex gap-5">
                     <Link href={'#'}>
-                        <Image className="hover:animate-shake" src="/assets/Page1/PILL.png" alt="" />
+                        <Image width={100} height={100} className="w-hover:animate-shake" src="/assets/Page1/PILL.png" alt="" />
                     </Link>
                     <Link href={'#'}>
-                        <Image className="hover:animate-shake" src="/assets/Page1/X.png" alt="" />
+                        <Image width={100} height={100} className="w-hover:animate-shake" src="/assets/Page1/X.png" alt="" />
                     </Link>
                 </div>
 
