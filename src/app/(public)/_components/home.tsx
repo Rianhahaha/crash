0e04e34252f44de3a82fc20f40f76c640db9ecb2
Rianhahaha <triandiapriliio@gmail.com>
2024-12-20 21:51:53 +0700

    first

--- src/app/(public)/_components/home.tsx
+++ src/app/(public)/_components/home.tsx
@@ -14,14 +14,14 @@ export default function Home() {
                     backgroundPosition: "center",
                 }}
             >
-                <img src="./assets/art.gif" className="bottom-0 left-0 absolute " alt="" />
-                <img src="./assets/title.png" className="top-[5%] right-[10%] absolute " alt="" />
+                <img src="/assets/art.gif" className="bottom-0 left-0 absolute " alt="" />
+                <img src="/assets/title.png" className="top-[5%] right-[10%] absolute " alt="" />
                 <div className="absolute bottom-[30%] right-[16%]  flex gap-5">
                     <Link href={'#'}>
-                        <img className="hover:animate-shake" src="./assets/Page1/PILL.png" alt="" />
+                        <img className="hover:animate-shake" src="/assets/Page1/PILL.png" alt="" />
                     </Link>
                     <Link href={'#'}>
-                        <img className="hover:animate-shake" src="./assets/Page1/X.png" alt="" />
+                        <img className="hover:animate-shake" src="/assets/Page1/X.png" alt="" />
                     </Link>
                 </div>
 
